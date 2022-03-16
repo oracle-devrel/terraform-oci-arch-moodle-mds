bf99f3b2bab2218e5a4f3c867f950fc8f153d3bd
lfeldman <lukasz.feldman@oracle.com>
2022-03-16 13:01:45 +0100

    Usage of remote module

--- orm/variables.tf
+++ orm/variables.tf
@@ -27,7 +27,7 @@ variable "availability_domain_number" {
 
 variable "release" {
   description = "Reference Architecture Release (OCI Architecture Center)"
-  default     = "1.0"
+  default     = "1.1"
 }
 
 variable "ssh_public_key" {
